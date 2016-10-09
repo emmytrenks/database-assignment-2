cdc5eac50f61b8dab66a4349768ea6c8b413bc0a
Emily Trenka <eat37@zips.uakron.edu>
2016-10-09 00:07:08 -0400

    Add fetch lib

---
+++ src/fetch.js
@@ -0,0 +1,37 @@
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) return response
+  else {
+    let error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function ajaxFetch(url, options = { }) {
+  return fetch(url, { credentials: 'same-origin', ...options }).then(checkStatus)
+}
+
+function getJSON(url) {
+  return ajaxFetch(url).then(res => res.json())
+}
+
+function postJSON(url, content = { }) {
+  return ajaxFetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(content)
+  })
+}
+
+function post(url, body, options = { }) {
+  return ajaxFetch(url, {
+    method: 'POST',
+    body,
+    ...options
+  })
+}
+
+export { getJSON, post, postJSON }
