8965b1797a9c76c9d4624775fe2d3d05f90493e0
Emily Trenka <eat37@zips.uakron.edu>
2016-12-04 20:16:43 -0500

    Add components

--- src/components/SalaryChange.js
+++ src/components/SalaryChange.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react'
+import numeral from 'numeral'
+import origin from 'origin-url'
+import { getJSON } from '../fetch'
 
 export default class extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      salaries: []
     }
   }
 
+  async componentDidMount() {
+    const salaries = await getJSON(`${origin}/api/salaries`)
+    this.setState({ salaries })
+  }
+
   render() {
+    const { salaries } = this.state
     return (
-      <div />
+      <div className="container">
+        <div className="row">
+          <div className="text-center">
+            <h2>Salary Change from 2014 to 2015</h2>
+          </div>
+          <div className="col-md-6 col-md-offset-3">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Salary Change</th>
+                </tr>
+              </thead>
+              <tbody>{salaries.filter(r => r.change !== 0).map(t => <tr key={t.id}>
+                <td>{t.first_name}</td>
+                <td>{t.last_name}</td>
+                <td style={{
+                  color: t.change > 0 ? 'green' : 'red'
+                }}>{numeral(t.change).format('$0.00a')}</td>
+              </tr>)}</tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/TeamMoney.js
+++ src/components/TeamMoney.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import numeral from 'numeral'
+import origin from 'origin-url'
+import { getJSON } from '../fetch'
 
 export default class extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      teams: []
     }
   }
 
+  async componentDidMount() {
+    const teams = await getJSON(`${origin}/api/teams`)
+    this.setState({ teams })
+  }
+
   render() {
+    const { teams } = this.state
     return (
-      <div />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Team</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>{teams.map(t => <tr key={t.team}><td>{t.team}</td><td>{numeral(t.total).format('$0.00a')}</td></tr>)}</tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     )
   }
 }
